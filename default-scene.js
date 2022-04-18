84942702a9b669138019bceaecda4afd484d7127
Daniel Pettersson <daniel.g.pettersson@gmail.com>
2022-04-18 21:29:04 +0200

    bright light

--- default-scene.js
+++ default-scene.js
@@ -1,6 +1,6 @@
 scene = scene(
     hittableList([
-        sphere(vec(10,30,0), 10, light(10, 10, 10)),
+        sphere(vec(10,30,0), 10, light(20, 20, 20)),
         sphere(vec(11.2,6.5,-1.4), .7, dielectric(solidColor(col(1, .6, .6)), 1.5)),
         sphere(vec(11.2,6.5, 1.3), .7, dielectric(solidColor(col(.6, 1, .6)), 1.5)),
         sphere(vec(0, 6, 5), 1.5, metal(solidColor(col(1, 1, .6)), .2)),
